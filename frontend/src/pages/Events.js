a79d31019805b088b634e6fe4e5ae0e1df742c0d
Dang Minh Tien <dangminhtien06@gmail.com>
2019-07-17 14:17:57 +0700

    Add modal to show question, infomation,...

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
-
+import "./Events.css";
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  cancelCreateEventHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  confirmCreateEventHandler = () => {};
+
   render() {
-    return <h1>The Event page</h1>;
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add event"
+            canCancel
+            canConfirm
+            onCancel={this.cancelCreateEventHandler}
+            onConfirm={this.onConfirm}
+          >
+            <p>Modal content</p>
+          </Modal>
+        )}
+
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 export default EventsPage;
