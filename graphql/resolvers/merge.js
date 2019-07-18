d9106d3e1a8dde85f521b77c809d3f2cd63aa3df
Dang Minh Tien <dangminhtien06@gmail.com>
2019-07-18 15:31:03 +0700

    Add Dataloader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,15 +1,25 @@
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const { dateToString } = require("../../helper/date");
+const DataLoader = require("dataloader");
+
+const eventLoader = new DataLoader(eventIds => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
       password: null,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      email: user.email,
+      createdEvents: () => eventLoader.loadMany(user._doc.createdEvents)
     };
   } catch (err) {
     throw err;
